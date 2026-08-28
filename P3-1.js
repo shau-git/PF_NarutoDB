5a310b6a473e60a10ac7db7e632b88c6d3ea0dbb
shau-git <111217322+shau-git@users.noreply.github.com>
2026-08-28 23:13:08 +0800

    remove link

---
+++ P3-1.js
@@ -0,0 +1,26 @@
+/*import {remedyImg2} from '../scripts/remedy.js'
+import {remedyImg} from '../scripts/data.js'
+import {fetch1,printObject} from '../scripts/render.js'*/
+
+
+//https://dattebayo-api.onrender.com/characters?limit=3000
+
+
+//to get the chracter id
+//https://dattebayo-api.onrender.com/characters/142
+
+const allChar = 'https://dattebayo-api.onrender.com/characters?limit=1430'
+fetch1(allChar,'characters').then(character => {
+                                                character.sort((a, b) => a.id - b.id) // sort the sequence by id                                                
+                                                const html = printObject(character)  //render.js
+                                                document.querySelector('.container').innerHTML = html      
+                            })
+                            .then(() =>remedyImg2()) //remedy.js
+                            .then(() => console.log('completed'));
+
+                           
+                            
+const soundsId = ['music-1','music-2','music-3','music-4','music-5']                           
+                            
+
+                            
---
+++ P5-1.js
@@ -0,0 +1,10 @@
+//import {fetch1,printObject} from '../scripts/render.js'
+
+const akaAll = 'https://dattebayo-api.onrender.com/akatsuki?limit=44'
+
+fetch1(akaAll,'akatsuki').then(character => {
+                                                character.sort((a, b) => a.id - b.id) // sort the sequence by id     
+                                                const html = printObject(character) ////render.js
+                                                document.querySelector('.container').innerHTML = html      
+                            })
+                            .then(() => console.log('completed'));
